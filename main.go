4a19f1d1ed0254e061df757a8d33c7baaa78b45e
AkhileshAdhikari <akhileshadhikaria1@gmail.com>
2024-06-01 23:21:24 +0545

    api-key

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import "net/http"
+
 func main() {
+	res, err := http.Get("http://api.weatherapi.com/v1/forecast.json?key=db03a6cec6f8441ba52173233240106q=")
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	println(res.StatusCode )
+	if res.StatusCode != 200 {
+		panic("api not available")
+	}
 	println("ready to go")
 }
