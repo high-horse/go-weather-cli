429186ca6a000232ad319066dd316436caa2c143
AkhileshAdhikari <akhileshadhikaria1@gmail.com>
2024-06-01 23:26:47 +0545

    example json

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
 
 func main() {
-	res, err := http.Get("http://api.weatherapi.com/v1/forecast.json?key=db03a6cec6f8441ba52173233240106q=")
+	res, err := http.Get("http://api.weatherapi.com/v1/forecast.json?key=db03a6cec6f8441ba52173233240106&q=london")
 	if err != nil {
 		panic(err)
 	}
 	defer res.Body.Close()
-	println(res.StatusCode )
+	// println(res.StatusCode )
 	if res.StatusCode != 200 {
 		panic("api not available")
 	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+println(string(body))
 	println("ready to go")
 }
