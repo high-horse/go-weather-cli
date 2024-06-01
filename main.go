7ae4c57841bb50aee9416643831e292cf9ac5733
AkhileshAdhikari <akhileshadhikaria1@gmail.com>
2024-06-01 23:35:17 +0545

    data map

--- main.go
+++ main.go
@@ -5,6 +5,32 @@ import (
 	"net/http"
 )
 
+type Weather struct {
+	Location struct {
+		Name string 	`json:"name"`
+		Country string 	`json:"country"`
+	}`json:"location"`
+	Current struct {
+		TempC float64 	`json:"temp_c"`
+		Condition struct{
+			Text string `json:"text"`
+		} 	`json:"condition"`
+	}`json:"current"`
+	Forecast struct {
+		Forecastday []struct {
+			Hour []struct {
+				TimeEpoch int64 `json:"time_epoch"`
+				TempC float64 	`json:"temp_c"`
+				Condition struct{
+					Text string `json:"text"`
+				} 	`json:"condition"`
+				ChanceOfRain float64 	`json:"chance_of_rain"`
+
+			} `json:"hour"`
+		}`json:"Forecastday"`
+	} `json:"forecast"`
+}
+
 func main() {
 	res, err := http.Get("http://api.weatherapi.com/v1/forecast.json?key=db03a6cec6f8441ba52173233240106&q=london")
 	if err != nil {
